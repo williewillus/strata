8cb21080cd3ab97c720d641ff420c932bbfd00a1
Strata Group <strata_group@example.com>
2019-12-08 20:40:20 +0000

    Add timings to perms_open

--- libfs/tests/perms_open.c
+++ libfs/tests/perms_open.c
@@ -5,48 +5,110 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/resource.h>
 #include <mlfs/mlfs_interface.h>
 
+static int do_time = 1;
+
+static long open_acc = 0;
+static long open_count = 0;
+static long create_acc = 0;
+static long create_count = 0;
+
+int my_open2(const char *path, int flags) {
+  int ret;
+  if (do_time) {
+    struct rusage before;
+    struct rusage after;
+    getrusage(RUSAGE_SELF, &before);
+    ret = open(path, flags);
+    getrusage(RUSAGE_SELF, &after);
+
+    if ((flags & O_CREAT) != 0) {
+      create_acc += (after.ru_utime.tv_usec - before.ru_utime.tv_usec);
+      create_count++;
+    } else {
+      open_acc += (after.ru_utime.tv_usec - before.ru_utime.tv_usec);
+      open_count++;
+    }
+  } else {
+    ret = open(path, flags);
+  }
+  return ret;
+}
+
+int my_open3(const char *path, int flags, mode_t mode) {
+  int ret;
+  if (do_time) {
+    struct rusage before;
+    struct rusage after;
+    getrusage(RUSAGE_SELF, &before);
+    ret = open(path, flags, mode);
+    getrusage(RUSAGE_SELF, &after);
+
+    if ((flags & O_CREAT) != 0) {
+      create_acc += (after.ru_utime.tv_usec - before.ru_utime.tv_usec);
+      create_count++;
+    } else {
+      open_acc += (after.ru_utime.tv_usec - before.ru_utime.tv_usec);
+      open_count++;
+    }
+  } else {
+    ret = open(path, flags, mode);
+  }
+  return ret;
+}
+
 static int test_create_rdonly() {
-    int fd = open("/mlfs/test_create_readonly", O_RDONLY|O_CREAT, S_IRUSR);
+    int fd = my_open3("/mlfs/test_create_readonly", O_RDONLY|O_CREAT, S_IRUSR);
     if (fd < 0) {
         perror("open with O_CREAT");
         return 0;
     }
     close(fd);
 
-    int fd2 = open("/mlfs/test_create_readonly", O_RDWR);
+    int fd2 = my_open2("/mlfs/test_create_readonly", O_RDWR);
     if (fd2 != -EACCES) {
       printf("Got result %d, expected -EACCES\n", fd2);
       return 0;
     }
 
-    fd2 = open("/mlfs/test_create_readonly", O_WRONLY);
+    fd2 = my_open2("/mlfs/test_create_readonly", O_WRONLY);
     if (fd2 != -EACCES) {
       printf("Got result %d, expected -EACCES\n", fd2);
       return 0;
     } 
 
+    if (unlink("/mlfs/test_create_readonly") != 0) {
+      perror("readonly cleanup");
+      return 0;
+    }
+
     return 1;
 }
 
 static int test_create_wronly() {
-    int fd = open("/mlfs/test_create_wronly", O_WRONLY|O_CREAT, S_IWUSR);
+    int fd = my_open3("/mlfs/test_create_wronly", O_WRONLY|O_CREAT, S_IWUSR);
     if (fd < 0) {
         perror("open with O_CREAT");
         return 0;
     }
     close(fd);
 
-    int fd2 = open("/mlfs/test_create_wronly", O_RDWR);
+    int fd2 = my_open2("/mlfs/test_create_wronly", O_RDWR);
     if (fd2 != -EACCES) {
       printf("Got result %d, expected -EACCES\n", fd2);
       return 0;
     }
 
-    fd2 = open("/mlfs/test_create_wronly", O_RDONLY);
+    fd2 = my_open2("/mlfs/test_create_wronly", O_RDONLY);
     if (fd2 != -EACCES) {
       printf("Got result %d, expected -EACCES\n", fd2);
+      return 0;
+    }
+
+    if (unlink("/mlfs/test_create_wronly") != 0) {
+      perror("wronly cleanup");
       return 0;
     }
 
@@ -54,33 +116,38 @@ static int test_create_wronly() {
 }
 
 static int test_create_rdwr() {
-    int fd = open("/mlfs/test_create_rdwr", O_RDWR|O_CREAT, S_IRUSR | S_IWUSR);
+    int fd = my_open3("/mlfs/test_create_rdwr", O_RDWR|O_CREAT, S_IRUSR | S_IWUSR);
     if (fd < 0) {
         perror("open with O_CREAT");
         return 0;
     }
     close(fd);
 
-    int fd2 = open("/mlfs/test_create_rdwr", O_RDWR);
+    int fd2 = my_open2("/mlfs/test_create_rdwr", O_RDWR);
     if (fd2 < 0) {
       printf("Got %d, expected valid fd (opening rdwr)\n", fd2);
       return 0;
     }
     close(fd2);
 
-    fd2 = open("/mlfs/test_create_rdwr", O_RDONLY);
+    fd2 = my_open2("/mlfs/test_create_rdwr", O_RDONLY);
     if (fd2 < 0) {
       printf("Got %d, expected valid fd (opening rdonly)\n", fd2);
       return 0;
     }
     close(fd2);
 
-    fd2 = open("/mlfs/test_create_rdwr", O_WRONLY);
+    fd2 = my_open2("/mlfs/test_create_rdwr", O_WRONLY);
     if (fd2 < 0) {
       printf("Got %d, expected valid fd (opening wronly)\n", fd2);
       return 0;
     }
     close(fd2);
+
+    if (unlink("/mlfs/test_create_rdwr") != 0) {
+      perror("rdwr cleanup");
+      return 0;
+    }
 
     return 1;
 }
@@ -88,6 +155,7 @@ static int test_create_rdwr() {
 
 int main(int argc, char ** argv)
 {
+ 
 	init_fs();
 	int fd = mkdir("/mlfs/", 0600);
 
@@ -102,7 +170,11 @@ int main(int argc, char ** argv)
 	  return 1;
 	}
 
-	printf("test_create_rdonly %s\n", test_create_rdonly() ? "succeeded" : "failed");
-	printf("test_create_wronly %s\n", test_create_wronly() ? "succeeded" : "failed");
-	printf("test_create_rdwr %s\n", test_create_rdwr() ? "succeeded" : "failed");
+        for (int i = 0; i < 15; i++) {
+	  printf("test_create_rdonly %s\n", test_create_rdonly() ? "succeeded" : "failed");
+	  printf("test_create_wronly %s\n", test_create_wronly() ? "succeeded" : "failed");
+	  printf("test_create_rdwr %s\n", test_create_rdwr() ? "succeeded" : "failed");
+        }
+	printf("Open timings: avg %.2f us\n", (float)open_acc / open_count);
+	printf("Create timings: avg %.2f us\n", (float)create_acc / create_count);
 }
